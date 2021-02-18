389ade4eef89b6e26b2909d14fc1d5cf8c158ff0
mattkyea <kyeamatt@gmail.com>
2021-02-18 15:40:35 -0500

    done with functionality

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 CS441 Assignment 1 - Hello World App
 
 Sources:
+All functions for TextView, which helped me realize what I could do with the text:
 https://developer.android.com/reference/android/widget/TextView?hl=en#public-methods
 
+Specific class of which an instance of is returned when trying to get the color of the text. Helped me understand how to access the color of the text:
+https://developer.android.com/reference/android/content/res/ColorStateList
+
+How to generate random hex numbers in Java:
+https://stackoverflow.com/questions/11094823/java-how-to-generate-a-random-hexadecimal-value-within-specified-range-of-value/48170421
--- app/src/main/java/com/example/cs441helloworld/MainActivity.java
+++ app/src/main/java/com/example/cs441helloworld/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView helloText;
@@ -28,33 +30,81 @@ public class MainActivity extends AppCompatActivity {
         boldButton = (Button) findViewById(R.id.bold_button);
         colorButton = (Button) findViewById(R.id.color_button);
         italicsButton = (Button) findViewById(R.id.italics_button);
-        moveButton = (Button) findViewById(R.id.move_button);
+        moveButton = (Button) findViewById(R.id.rotate_button);
         capsButton =(Button) findViewById(R.id.caps_button);
 
     }
 
-    //called when button is pressed
+    //all of my functions modify the text based on what the button says
+    //most toggle between the default and the modifier
+    //some are random (color toggles between black and random color)
+
+    //called when color button is pressed
     public void onColorClick(View view) {
-        //make random color from random num
-        helloText.setTextColor(0xFFFFFF00);
+        int currentColor = helloText.getTextColors().getDefaultColor();//how to access color from ColorStateList
+        if(currentColor == 0xFF000000){//text is black, make random color from random num
+            //from https://stackoverflow.com/questions/11094823/java-how-to-generate-a-random-hexadecimal-value-within-specified-range-of-value/48170421
+            Random randomGenerator = new Random();
+            //colors are just ORGB values - opacity, red, green, blue, each is a 2-digit hex number
+            //FF for opacity means its 100% opaque, and we can see it
+            //so when generating random numbers, I ensure we can see it (lowest possible number is 0xFF000000 - opaque black)
+            int randomHexInt = randomGenerator.nextInt(0xFFFFFF) + 0xFF000000;//generates a random number between 0xFF000000 and 0xFFFFFFFF (opaque black to opaque white)
+            helloText.setTextColor(randomHexInt);
+        }else { //text isn't opaque black - the default - so reset it
+            helloText.setTextColor(0xFF000000);
+        }
     }
 
-    //called when button is pressed
+    //called when bold button is pressed
     public void onBoldClick(View view) {
-        helloText.setTypeface(Typeface.MONOSPACE, Typeface.BOLD);
+        //text can be bold, italics, both, or neither
+        //but to make both, we need special modifier of Typeface.BOLD_ITALICS, thus requiring a check for each possible case
+        if(helloText.getTypeface().getStyle() == Typeface.BOLD_ITALIC){//italics and bold
+            helloText.setTypeface(Typeface.SANS_SERIF, Typeface.ITALIC);
+        }else if(helloText.getTypeface().getStyle() == Typeface.BOLD && helloText.getTypeface().getStyle() != Typeface.ITALIC){//bold, not italics
+            helloText.setTypeface(Typeface.SANS_SERIF, Typeface.NORMAL);
+        }else if(helloText.getTypeface().getStyle() != Typeface.BOLD && helloText.getTypeface().getStyle() == Typeface.ITALIC){//italics, not bold
+            helloText.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD_ITALIC);
+        }else{//not bold or italics
+            helloText.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD);
+        }
     }
 
-    //called when button is pressed
+    //called when italics button is pressed
     public void onItalicsClick(View view) {
-        helloText.setTypeface(Typeface.MONOSPACE, Typeface.ITALIC);
+        //same as above, but with toggling italics and leaving bold as is
+        if(helloText.getTypeface().getStyle() == Typeface.BOLD_ITALIC){//bold and italics
+            helloText.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD);
+        }else if(helloText.getTypeface().getStyle() == Typeface.BOLD && helloText.getTypeface().getStyle() != Typeface.ITALIC){//bold, not italics
+            helloText.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD_ITALIC);
+        }else if(helloText.getTypeface().getStyle() != Typeface.BOLD && helloText.getTypeface().getStyle() == Typeface.ITALIC){//italics, not bold
+            helloText.setTypeface(Typeface.SANS_SERIF, Typeface.NORMAL);
+        }else{//not bold or italics
+            helloText.setTypeface(Typeface.SANS_SERIF, Typeface.ITALIC);
+        }
     }
-    //called when button is pressed
-    public void onMoveClick(View view) {
-        //figure out how
+    //called when rotate button is pressed
+    public void onRotateClick(View view) {
+        //pick random x, y, z to rotate the text to
+        if(helloText.getRotationX() ==0 && helloText.getRotationY() ==0 && helloText.getRotation() == 0){//at default - no rotation
+            //pick 3 random numbers between 0 and 360 so set as rotation values
+            Random randomGenerator = new Random();
+            helloText.setRotationX(randomGenerator.nextInt(360));
+            helloText.setRotationY(randomGenerator.nextInt(360));
+            helloText.setRotation(randomGenerator.nextInt(360));
+        }else{//otherwise we're in some random position, reset
+            helloText.setRotationX(0);
+            helloText.setRotationY(0);
+            helloText.setRotation(0);
+        }
     }
 
-    //called when button is pressed
+    //called when caps button is pressed
     public void onCapsClick(View view) {
-        helloText.setAllCaps(true);
+        if(helloText.isAllCaps() == true) {//if text is caps
+            helloText.setAllCaps(false);//switch to lowercase
+        }else {//text isn't caps, make it caps
+            helloText.setAllCaps(true);
+        }
     }
 }
